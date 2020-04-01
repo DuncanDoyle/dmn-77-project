e68e85439a5f82c279df56db44d3a5d54c1b5c08
pamAdmin <>
2020-04-01 14:49:02 +0200

    {/src/main/java/com/myspace/dmn_77_project/Person.java}

--- src/main/java/com/myspace/dmn_77_project/Person.java
+++ src/main/java/com/myspace/dmn_77_project/Person.java
@@ -6,12 +6,35 @@ package com.myspace.dmn_77_project;
 
 public class Person implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Person() {
-    }
+	@org.kie.api.definition.type.Label(value = "Name")
+	private java.lang.String name;
+	@org.kie.api.definition.type.Label(value = "Age")
+	private int age;
 
+	public Person() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Person(java.lang.String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 
 }
